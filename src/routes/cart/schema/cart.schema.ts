ecdd81a8937d77ee41a93f03caed2a219e3f24b9
Yugsingh05 <yugsingh7622@gmail.com>
2025-07-18 00:20:22 +0530

    feat : added the fontstyle adn customization in the cart

--- src/routes/cart/schema/cart.schema.ts
+++ src/routes/cart/schema/cart.schema.ts
@@ -1,33 +1,37 @@
 import { z } from "zod";
 import zodToJsonSchema from "zod-to-json-schema";
 
-
 export const cartSchema = z.object({
-    userId : z.string().nonempty(),
-    productId: z.string().uuid().nonempty(),
-    quantity: z.number().min(1).default(1),
-})
-
+  userId: z.string().nonempty(),
+  productId: z.string().uuid().nonempty(),
+  quantity: z.number().min(1).default(1),
+});
 
 export const cartResponseSchema = z.object({
-    id: z.string(),
-    userId: z.string().nonempty(),
-    productId: z.string().uuid().nonempty(),
-    quantity: z.number().min(1).default(1),
-    createdAt: z.string().datetime(),
-    updatedAt: z.string().datetime(),
-})
+  id: z.string(),
+  userId: z.string().nonempty(),
+  productId: z.string().uuid().nonempty(),
+  quantity: z.number().min(1).default(1),
+  fontStyle: z.string().optional(),
+  customizationName: z.string().optional(),
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime(),
+});
 
 export const cartBodySchema = zodToJsonSchema(cartSchema);
 
 export const cartResponseSchemaObject = zodToJsonSchema(cartResponseSchema);
 
-export const cartUpdateProduct  = zodToJsonSchema(cartSchema.partial());
+export const cartUpdateProduct = zodToJsonSchema(cartSchema.partial());
 
-export const LikeBodySchema = zodToJsonSchema(cartSchema.omit({
-    quantity: true
-}));
+export const LikeBodySchema = zodToJsonSchema(
+  cartSchema.omit({
+    quantity: true,
+  })
+);
 
-export const LikeResponseSchema = zodToJsonSchema(cartResponseSchema.omit({
-    quantity: true
-}));
+export const LikeResponseSchema = zodToJsonSchema(
+  cartResponseSchema.omit({
+    quantity: true,
+  })
+);
